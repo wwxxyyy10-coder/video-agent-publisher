b560d3a11604b59eb515c82157dca02e5d37c67b
wwxxyyy10-coder <wwxxyyy10@gmail.com>
2026-08-25 06:51:33 +0300

    إضافة نقطة الدخول الرئيسية

---
+++ __main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+نقطة الدخول الرئيسية - Entry Point
+استخدم: python cli.py أو python -m video_agent
+"""
+
+import sys
+from cli import cli
+
+if __name__ == '__main__':
+    sys.exit(cli())
